1f67a96ac2d19e92d1a95c6fa49c2582607c42a3
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2024-09-15 07:43:56 +0530

    formatting + added code to optimize training

--- src/download_images.py
+++ src/download_images.py
@@ -1,43 +1,53 @@
 import os
-import requests
-from concurrent.futures import ThreadPoolExecutor
-from tqdm import tqdm
-import pandas as pd
 import re
+from concurrent.futures import ThreadPoolExecutor
+
+import pandas as pd
+import requests
+from tqdm import tqdm
+
 
 def extract_unique_filename(url):
     # Extract the unique filename using regex
-    match = re.search(r'/([A-Za-z0-9]+)\.jpg$', url)
+    match = re.search(r"/([A-Za-z0-9]+)\.jpg$", url)
     if match:
         return match.group(1)
     else:
         return url.split("/")[-1]  # Fallback to the old method if regex fails
+
 
 def download_image(url, SAVE_DIR):
     try:
         img_data = requests.get(url, timeout=5).content
         unique_filename = extract_unique_filename(url)
         img_name = os.path.join(SAVE_DIR, f"{unique_filename}.jpg")
-        with open(img_name, 'wb') as handler:
+        with open(img_name, "wb") as handler:
             handler.write(img_data)
         return url, True
     except Exception as e:
         return url, False
 
+
 def download_images_in_parallel(image_urls, SAVE_DIR):
     with ThreadPoolExecutor(max_workers=10) as executor:
-        results = list(tqdm(executor.map(lambda url: download_image(url, SAVE_DIR), image_urls), total=len(image_urls)))
+        results = list(
+            tqdm(
+                executor.map(lambda url: download_image(url, SAVE_DIR), image_urls),
+                total=len(image_urls),
+            )
+        )
     return results
 
+
 if __name__ == "__main__":
-    SAVE_DIR = '../images'
-    DATASET_FOLDER = 'dataset/'
+    SAVE_DIR = "images/"
+    DATASET_FOLDER = "dataset/"
 
     if not os.path.exists(SAVE_DIR):
         os.makedirs(SAVE_DIR)
 
-    train_data = pd.read_csv(os.path.join(DATASET_FOLDER, 'train.csv'))
-    image_urls = train_data['image_link'].tolist()
+    train_data = pd.read_csv(os.path.join(DATASET_FOLDER, "train.csv"))
+    image_urls = train_data["image_link"].tolist()
 
     results = download_images_in_parallel(image_urls, SAVE_DIR)
 
